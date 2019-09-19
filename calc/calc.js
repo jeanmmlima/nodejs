e4407c0941a46c581cc3125fec1c1a3ebec4f30e
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-19 09:49:06 -0300

    calc with js

--- calc/calc.js
+++ calc/calc.js
@@ -2,5 +2,11 @@
 
 //carregando modulos
 var somaFunc = require("./funcs/somar")
+var subFunc = require("./funcs/sub")
+var multiFunc = require("./funcs/multi")
+var divFunc = require("./funcs/div")
 
 console.log(somaFunc(2,4))
+console.log(subFunc(6,1))
+console.log(multiFunc(6,6))
+console.log(divFunc(1,23))
