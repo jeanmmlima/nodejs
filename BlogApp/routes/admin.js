8509630257cb4f7832d10bee607a563d44a679ab
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-30 11:19:22 -0300

    inserindo categorias

--- BlogApp/routes/admin.js
+++ BlogApp/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require('mongoose')
+require("../models/Categoria")
+const Categorias = mongoose.model("categorias")
 
 router.get('/',(req,res) =>{
     //renderiza a view/admin/index
@@ -13,6 +16,19 @@ router.get('/posts', (req,res) => {
 router.get('/categorias',(req,res) => {
    // res.send("Página de categorias")
    res.render("admin/categorias")
+})
+
+router.post("/categorias/nova",(req,res) => {
+    const novaCategoria = {
+        //name dos input texts
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+    new Categorias(novaCategoria).save().then(() => {
+        console.log("Categoria salva com sucesso")
+    }).catch((err) => {
+        console.log("Erro ao inserir categoria: "+err)
+    })
 })
 
 router.get('/categorias/add',(req,res) => {
