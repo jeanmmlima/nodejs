d4fae81884017ea7cf28c9632eb023c306652c06
jeanmmlima <jeanmariomlima@gmail.com>
2019-10-01 09:50:02 -0300

    middlewares, categoria validations

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -10,7 +10,28 @@ const admin = require('./routes/admin')
 const path = require("path")
 const mongoose = require('mongoose')
 
+const session = require("express-session")
+const flash = require("connect-flash")
+
 //2. Settings
+    //2.0 Session
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+
+    }))
+    app.use(flash())
+
+    //2.0.1 Middleware para trabalhar com sessoes
+    app.use((req,res,next) => {
+        //possível guardar variaveis globias
+        //res.locals.nome = "Meu nome"
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+
     //2.1 Body Parsers
     app.use(bodyParsers.urlencoded({extended: true}))
     app.use(bodyParsers.json())
@@ -31,6 +52,13 @@ const mongoose = require('mongoose')
     //2.4 Public files - arquivos estaticos em public
     app.use(express.static(path.join(__dirname,"public")))
     //app.use(express.static('public'))
+
+    //MIDDLEWARE
+    app.use((req,res,next) => {
+        console.log("OLA EU SOU UM MIDDLEWARE")
+        //passa a requisicao
+        next()
+    })
 
 //3. Routes
     //Parâmetros: prefixo das rotas, constante definida
--- BlogApp/routes/admin.js
+++ BlogApp/routes/admin.js
@@ -19,16 +19,40 @@ router.get('/categorias',(req,res) => {
 })
 
 router.post("/categorias/nova",(req,res) => {
-    const novaCategoria = {
-        //name dos input texts
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    //validacao
+    var erros = [];
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido!"})
     }
-    new Categorias(novaCategoria).save().then(() => {
-        console.log("Categoria salva com sucesso")
-    }).catch((err) => {
-        console.log("Erro ao inserir categoria: "+err)
-    })
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido!"})
+    }
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria é muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias",{erros: erros})
+    } else {
+
+        const novaCategoria = {
+            //name dos input texts
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        new Categorias(novaCategoria).save().then(() => {
+            //console.log("Categoria salva com sucesso")
+            req.flash("success_msg", "Categoria criada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve erro ao salvar a categoria. Tente novamente!")
+           // console.log("Erro ao inserir categoria: "+err)
+           res.redirect("/admin")
+        })
+    }
+
 })
 
 router.get('/categorias/add',(req,res) => {
