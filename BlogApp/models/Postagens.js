636a43a39af3d5e4b62d5e18cc8ae94d7f207e99
jeanmmlima <jeanmariomlima@gmail.com>
2019-10-04 10:08:16 -0300

    model de postagens

---
+++ BlogApp/models/Postagens.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const Postagem = new Schema({
+    titulo:{
+        type: String,
+        required: true,
+    },
+    slug:{
+        type: String,
+        required: true
+    },
+    descricao:{
+        type: String,
+        required: true
+    },
+    conteudo:{
+        type: String,
+        required: true
+    },
+    categoria:{
+        //categoria armazeda Id de algum objeto, no caso categoria
+        type: Schema.Types.ObjectId,
+        //nome do model
+        ref: "categorias",
+        required: true
+    },
+    data:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+mongoose.model("postagens", Postagem)
